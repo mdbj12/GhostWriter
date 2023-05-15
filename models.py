72e6af257ecafa40b60c9443a0593aa36f8521d6
Michael Jeung <michaelj229@gmail.com>
2023-05-15 16:07:08 -0400

    importing sql

---
+++ models.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
