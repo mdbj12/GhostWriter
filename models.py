aca7746777119ab77ada283d733eeb1ee2840b26
Michael Jeung <michaelj229@gmail.com>
2023-05-16 14:18:57 -0400

    refactored models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Table, Boolean, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
@@ -17,13 +17,13 @@ class User(Base):
     phone_number = Column('phone number', Integer)
 
     def __repr__(self):
-        return f"""
-                name: {self.name},
-                age:{self.age},
-                fav_genre:{self.fav_genre},
-                email:{self.email},
-                phone_number:{self.phone_number}
-                """
+        return f'''
+            name: {self.name},
+            age:{self.age},
+            fav_genre:{self.fav_genre},
+            email:{self.email},
+            phone_number:{self.phone_number}
+        '''
     
 
 class Books(Base):
@@ -37,13 +37,13 @@ class Books(Base):
     publish_date = Column('date', String())
     read = Column('read', Boolean())
 
-    def __init__(self, title, author, publish_date, read):
-        self.title = title
-        self.author = author
-        self.publish_date = publish_date
-        self.read = read
-
-        Books.all.append(self)
+    def __repr__(self):
+        return f'''
+            title: {self.title},
+            author: {self.author},
+            publish_date: {self.publish_date},
+            read; {self.read}
+        '''
 
 class Reviews(Base):
     __tablename__ = 'reviews'
@@ -54,11 +54,11 @@ class Reviews(Base):
     text = Column('review', String())
     rating = Column('rating out of 5', Integer())
 
-    def __init__(self, text, rating):
-        self.text = text
-        self.rating = rating
-
-        Reviews.all.append(self)
+    def __repr__(self):
+        return f'''
+            text: {self.text},
+            rating: {self.rating}
+        '''
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///app.db')
